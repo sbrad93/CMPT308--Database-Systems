a2a81384863c0de32fe39e16976767eadb533b4e
Shannon Brady <shannonbrady@Shannons-MacBook-Air.local>
2020-09-16 12:12:43 -0400

    Shannon Brady Lab 3 (09/16/20) Final

--- Brady.Lab3.sql
+++ Brady.Lab3.sql
@@ -1,7 +1,7 @@
 -- Shannon Brady
 -- Professor Labouseur
 -- CMPT308
--- 12 September 2020
+-- 11 September 2020
 -- Lab 3
 
 
@@ -25,39 +25,33 @@ from people
 where dob between '1950-01-01' and '1960-01-01';
 
 -- 5.
--- dr. peart, ms. schock, ms. plakas, ms. carrington, dr. bruford
 select prefix, lastname
 from people
 where prefix != 'Mr.';
 
 -- 6.
--- p02, p09
 select *
 from products
 where city != 'Dallas' and city != 'Duluth' 
     and priceusd >= 3;
 
 -- 7.
--- 1022, 1023, 1024
 select *
 from orders
 where dateordered between '2020-03-01' and '2020-03-31';
 
 -- 8.
--- 1017, 1018, 1021
 select *
 from orders
 where dateordered between '2020-02-01' and '2020-02-29' 
 	and totalusd >= 20000;
     
 -- 9.
--- none
 select *
 from orders
 where custid = 007;
 
 -- 10.
--- 1015, 1024
 select *
 from orders
 where custid = 005;
